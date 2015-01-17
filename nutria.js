601700562d3af84142c6527194dbc44a9a530af2
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2015-01-16 19:07:04 -0500

    Full setup + upload in Readme

--- nutria.js
+++ nutria.js
@@ -1,41 +1,11 @@
 Products = new Mongo.Collection("products");
 
 if (Meteor.isClient) {
-    Template.data.helpers({
-        loaded: function() {
-            if (!Session.get("new_uploaded")) {
-                var result = !Products.findOne({});
-                if (result) {
-                    Session.set("new_uploaded", 1);
-                }
-            }
-            Session.set("new_loaded", 1);
-            return true;
-        },
-        products_not_exist: function() {
-            if (top.location.hash.indexOf('upload') > -1) {
-                return true;
-            }
-
-            var result = !Session.get("new_uploaded");
-            var first;
-            if (result) {
-                first = !Products.findOne({});
-                if (first) {
-                    Session.set("new_uploaded", 1);
-                    result = false;
-                }
-            }
-            Session.set("new_loaded", 1);
-            return result;
-        }
-    });
-
     Template.products.helpers({
         products: function() {
             var search = Session.get("search");
             if (search && search.length > 1) {
-                return Products.find({"Shrt_Desc": {$regex: '^' + search + '.*', $options: 'i'}});
+                return Products.find({"Shrt_Desc": {$regex: search, $options: 'i'}});
             } else {
                 return []
             }
@@ -69,29 +39,6 @@ if (Meteor.isClient) {
     Template.search.events({
         'keyup input': function(e, template) {
             Session.set("search", event.target.value);
-        }
-    });
-
-    Template.upload.events({
-        'submit form': function (e, template) {
-            e.preventDefault();
-            var file = $('form.upload input[type=file]').get(0).files[0];
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var csv = e.target.result;
-                var result = Papa.parse(csv);
-                var captions = result.data.splice(0, 1);  // Using captions
-                _.each(result.data, function(row) {
-                    var obj = _.object(captions[0], row);
-                    if (obj.Shrt_Desc) {
-                        Products.insert(obj);
-                        console.log('Inserted ', obj.Shrt_Desc);
-                    } else {
-                        console.log('Skipped ', obj);
-                    }
-                });
-            };
-            reader.readAsText(file);
         }
     });
 }
